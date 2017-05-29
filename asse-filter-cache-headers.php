4a74f549d0ff4f9ae4b2cd18737a2d0c57827949
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-05-29 12:23:40 +0200

    ~ minor fix

--- asse-filter-cache-headers.php
+++ asse-filter-cache-headers.php
@@ -100,7 +100,7 @@ class Asse_Cache_Control {
         $directives = null;
 
         if ( ! $this->should_be_cached() ) {
-            $directives = get_cache_control_directive();
+            $directives = $this->get_cache_control_directive();
         }
 
         if ( $wp_query->is_front_page() && ! is_paged() ) {
